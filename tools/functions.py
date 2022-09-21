664f5ad2d5b5170c807b60cddf554ea03daa75de
Kodlak15 <stanlcod15@gmail.com>
2022-09-20 22:01:02 -0700

    add functions.py

--- tools/functions.py
+++ tools/functions.py
@@ -3,11 +3,22 @@ from sympy import Expr, Symbol
 from string import ascii_letters as letters
 from typing import Dict, List, Tuple, Union, Any 
 
-### UNFINISHED ###
-
 class Function:
     """
-    ToDo
+    This class utilizes the sympy library to provide a convenient way to work with functions
+    Some of the functionality I would like to implement for this class includes:
+        - evaluation (done)
+        - differentiation (not done)
+        - integration (mostly done, but more functionality will be added as course continues)
+        - change of variables (not done)
+        - etc.
+
+    f: An expression involving sympy symbols
+
+    ex:
+        x = Symbol('x')
+        y = Symbol('y')
+        f = Function(x**2 + y**2)
     """
     def __init__(self, f: Expr) -> None:
         self.f = f.simplify()
@@ -21,19 +32,10 @@ class Function:
     def __call__(self, subs: Dict[Symbol, Union[Symbol, float]] = {}) -> Any:
          return self.f.subs(subs)
 
-    @property
-    def integrand(self) -> Expr:
-        """
-        Todo
-        """
-        integrand = self.f.copy()
-        for d in self.diffs.values():
-            integrand *= d
-        return integrand.simplify()
-
     def undo(self) -> None:
         """
-        ToDo
+        Undoes the last operation that altered the state of the function
+        Replaces the current state with the most recent state saved in the memory array
         """
         assert len(self.memory) > 0, "Nothing to undo"
         self.f = self.memory.pop(-1)
@@ -44,7 +46,10 @@ class Function:
         interval: Union[Tuple[float, float], None] = None,
         ) -> Expr:
         """
-        (ToDo)
+        Computes the integral of the function
+
+        var: The sympy symbol representing the variable of integration
+        interval: The interval to integrate over (if applicable)
         """
         if not interval:
             return self.f.integrate(var).simplify()
@@ -57,7 +62,16 @@ class Function:
         intervals: Union[List[Tuple[float, float]], None] = None,
         ) -> None:
         """
-        ToDo
+        Integrates the function
+        This operation alters the state of the function, to reset the state call self.undo()
+        It is the users responsibility to set up the order of integration correctly, but here are a couple guidelines
+            1. make sure variables and their bounds of integration are entered in the same order
+                - variables = [x, y, z]
+                - intervals = [x_interval, y_interval, z_interval]
+            2. if you want this function to return a number, integrate over all the variables and make sure the last interval does not depend on any variables
+
+        variables: A list of the variables of integration
+        intervals: A list of the intervals to integrate over
         """
         self.memory.append(self.f)
         if not intervals:
